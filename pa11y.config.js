f311abf2e7ea6f5921083c099cfe8bafaa68254a
Kataneki <gavintaylor.ai@gmail.com>
2025-12-24 02:38:37 -0600

    test: ci

--- pa11y.config.js
+++ pa11y.config.js
@@ -1,18 +1,11 @@
-module.exports = {
-  // URL or file to test will be passed from the CLI, so don't hardcode here
-  standard: 'WCAG2AA',           // Level of accessibility compliance
-  timeout: 30000,                 // 30 seconds
-  ignore: [
-    'notice',                     // Ignore minor notices
-    'warning',                    // Ignore warnings if you want
-  ],
+export default {
+  standard: 'WCAG2AA',
+  timeout: 30000,
+  ignore: ['notice', 'warning'],
   chromeLaunchConfig: {
-    args: [
-      '--no-sandbox',             // Required for CI environments like GitHub Actions
-      '--disable-setuid-sandbox', // Required for CI environments
-    ],
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
   },
   log: {
-    debug: console.log,           // Log debug messages
+    debug: console.log,
   },
 };
